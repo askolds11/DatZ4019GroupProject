797f4693bccdeb01b11ebfc668c57368e6fff980
jjeris <johans.justs.eris@gmail.com>
2024-05-02 21:38:57 +0300

    Add broken Event repository

---
+++ app/src/main/java/com/grupacetri/oopprojekts/featureEvent/data/EventRepository.kt
@@ -0,0 +1,12 @@
+package com.grupacetri.oopprojekts.featureEvent.data
+
+import com.grupacetri.oopprojekts.Foo
+import kotlinx.coroutines.flow.Flow
+
+interface EventRepository {
+//    fun delete(id: Long)
+//    fun insert(foo: Long?)
+
+//    fun insert(data: Type?, data: Type?, data: Type?)
+//    fun update(data: Type?, data: Type?, data: Type?)
+}
---
+++ app/src/main/java/com/grupacetri/oopprojekts/featureEvent/data/EventRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.grupacetri.oopprojekts.featureEvent.data
+
+import com.grupacetri.oopprojekts.Database
+
+class EventRepositoryImpl (
+    private val database: Database
+): EventRepository {
+
+
+//    override fun insert() {
+//        database.eventQueries.insert()
+//    }
+//    override fun update() {
+//        database.eventQueries.insert()
+//    }
+//    override fun delete(id: Long) {
+//        database.fooQueries.delete(id)
+//    }
+}
+
