e6b52037577713e03eff1b57b7f3d69438075247
askolds <askolds11@gmail.com>
2024-05-13 15:12:44 +0300

    Fix bugs, comment code

--- app/src/main/java/com/grupacetri/oopprojekts/featureFoo/ui/FooScreenState.kt
+++ app/src/main/java/com/grupacetri/oopprojekts/featureFoo/ui/FooScreenState.kt
@@ -4,9 +4,11 @@ import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import com.grupacetri.oopprojekts.featureFoo.domain.FooItem
+import kotlin.random.Random
 
 @Stable
 class FooScreenState {
     val fooList = mutableStateListOf<FooItem>()
     val inputText = mutableStateOf("")
+    val randomText = mutableStateOf(Random.nextInt().toString())
 }
