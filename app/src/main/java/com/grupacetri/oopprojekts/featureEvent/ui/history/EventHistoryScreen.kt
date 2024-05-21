fcd679b652e2ea91a91623f55d2fc404e29aa0d0
askolds <askolds11@gmail.com>
2024-05-21 17:12:03 +0300

    Fix code from rebase

--- app/src/main/java/com/grupacetri/oopprojekts/featureEvent/ui/history/EventHistoryScreen.kt
+++ app/src/main/java/com/grupacetri/oopprojekts/featureEvent/ui/history/EventHistoryScreen.kt
@@ -4,42 +4,34 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.grupacetri.oopprojekts.core.collectAsStateWithLifecycle
-import com.grupacetri.oopprojekts.core.ui.navigation.NavigateToRoute
-import com.grupacetri.oopprojekts.core.ui.navigation.NavigationRoute
-import com.grupacetri.oopprojekts.core.ui.theme.OOPProjektsTheme
-import com.grupacetri.oopprojekts.featureEvent.domain.EventItem
+import com.grupacetri.oopprojekts.core.ui.navigation.EventNavigationRoute
+import com.grupacetri.oopprojekts.core.ui.navigation.NavigateToRoute2
 import me.tatarka.inject.annotations.Assisted
 import me.tatarka.inject.annotations.Inject
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 
-typealias EventHistoryScreen = @Composable (navigate: NavigateToRoute) -> Unit
+typealias EventHistoryScreen = @Composable (navigate: NavigateToRoute2) -> Unit
 
 @Inject
 @Composable
 fun EventHistoryScreen(
     eventViewModel: () -> EventHistoryScreenViewModel,
-    @Assisted navigate: NavigateToRoute
+    @Assisted navigate: NavigateToRoute2
 ) {
     val viewModel = viewModel { eventViewModel() }
     viewModel.eventHistoryFlow.collectAsStateWithLifecycle()
 
     // clear navigation
 //    LaunchedEffect(Unit) {
-//        viewModel.onEvent(EventListScreenEvent.NavigateToRoute(null))
+//        viewModel.onEvent(EventListScreenEvent.NavigateToRoute2(null))
 //    }
 
     ExampleContent(
@@ -53,13 +45,13 @@ fun EventHistoryScreen(
 private fun ExampleContent(
     state: EventHistoryScreenState,
     onEvent: (EventHistoryScreenEvent) -> Unit,
-    navigate: NavigateToRoute
+    navigate: NavigateToRoute2
 ) {
     var nav_var by remember {
         mutableStateOf(false)
     }
     if (nav_var) {
-        navigate(NavigationRoute.Event)
+        navigate(EventNavigationRoute.Event)
     }
     LazyColumn {
         items(state.eventHistoryList) {
