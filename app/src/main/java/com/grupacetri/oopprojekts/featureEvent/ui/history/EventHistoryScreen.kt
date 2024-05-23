e4ca44193d565a76ca15a59c15a60d3f6a85ff3e
EBergs <edijs333@gmail.com>
2024-05-23 21:48:39 +0300

    Clickable history and id to record for edit

--- app/src/main/java/com/grupacetri/oopprojekts/featureEvent/ui/history/EventHistoryScreen.kt
+++ app/src/main/java/com/grupacetri/oopprojekts/featureEvent/ui/history/EventHistoryScreen.kt
@@ -1,20 +1,20 @@
 package com.grupacetri.oopprojekts.featureEvent.ui.history
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.grupacetri.oopprojekts.core.collectAsStateWithLifecycle
-import com.grupacetri.oopprojekts.core.ui.navigation.EventNavigationRoute
+import com.grupacetri.oopprojekts.core.ui.navigation.FooNavigationRoute
 import com.grupacetri.oopprojekts.core.ui.navigation.NavigateToRoute2
+import com.grupacetri.oopprojekts.core.ui.sideeffect.SideEffectComposable
 import me.tatarka.inject.annotations.Assisted
 import me.tatarka.inject.annotations.Inject
 
@@ -29,37 +29,38 @@ fun EventHistoryScreen(
     val viewModel = viewModel { eventViewModel() }
     viewModel.eventHistoryFlow.collectAsStateWithLifecycle()
 
-    // clear navigation
-//    LaunchedEffect(Unit) {
-//        viewModel.onEvent(EventListScreenEvent.NavigateToRoute2(null))
-//    }
+    SideEffectComposable(viewModel) {
+        when(it) {
+            is EventHistoryScreenEvent.SideEffectEvent.NavigateToScreen999 -> {
+                navigate(FooNavigationRoute.Foo)
+                it.id
+            }
+        }
+    }
 
-    ExampleContent(
+    EventHistoryScreenContent(
         viewModel.state,
-        {},
-        navigate
+        viewModel::onEvent
     )
 }
 
 @Composable
-private fun ExampleContent(
+private fun EventHistoryScreenContent(
     state: EventHistoryScreenState,
     onEvent: (EventHistoryScreenEvent) -> Unit,
-    navigate: NavigateToRoute2
 ) {
-    var nav_var by remember {
-        mutableStateOf(false)
-    }
-    if (nav_var) {
-        navigate(EventNavigationRoute.Event)
-    }
+
     LazyColumn {
         items(state.eventHistoryList) {
-            Row {
+            Row (modifier = Modifier
+                    .clickable { onEvent(EventHistoryScreenEvent.SideEffectEvent.NavigateToScreen999(it.id))}
+                    .padding(horizontal = 12.dp, vertical = 8.dp)
+                ){
                 Text(it.name)
                 Text(it.time_created)
                 Text(it.time_ended)
                 Spacer(modifier = Modifier.weight(1f))
+
 //                Button(
 //                    onClick = { onEvent(EventHistoryScreenEvent.StartTracking(it.id)) }
 //                ) {
