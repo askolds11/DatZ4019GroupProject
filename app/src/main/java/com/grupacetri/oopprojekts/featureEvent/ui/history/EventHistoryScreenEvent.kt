834f82e7b1a3a1b1e0829e9870124a9cb8f51530
EBergs <edijs333@gmail.com>
2024-05-23 21:48:39 +0300

    Clickable history and id to record for edit

--- app/src/main/java/com/grupacetri/oopprojekts/featureEvent/ui/history/EventHistoryScreenEvent.kt
+++ app/src/main/java/com/grupacetri/oopprojekts/featureEvent/ui/history/EventHistoryScreenEvent.kt
@@ -1,7 +1,10 @@
 package com.grupacetri.oopprojekts.featureEvent.ui.history
 
+import com.grupacetri.oopprojekts.core.ui.sideeffect.BaseSideEffectEvent
 
 
 sealed class EventHistoryScreenEvent {
-
+    sealed class SideEffectEvent: BaseSideEffectEvent, EventHistoryScreenEvent() {
+        data class NavigateToScreen999(val id: Long): SideEffectEvent()
+    }
 }
--- app/src/main/java/com/grupacetri/oopprojekts/featureEvent/ui/history/EventHistoryScreenViewModel.kt
+++ app/src/main/java/com/grupacetri/oopprojekts/featureEvent/ui/history/EventHistoryScreenViewModel.kt
@@ -1,7 +1,7 @@
 package com.grupacetri.oopprojekts.featureEvent.ui.history
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.grupacetri.oopprojekts.core.ui.sideeffect.SideEffectViewModel
 import com.grupacetri.oopprojekts.featureEvent.di.EventScope
 import com.grupacetri.oopprojekts.featureEvent.domain.EventUseCases
 import kotlinx.coroutines.flow.SharedFlow
@@ -14,7 +14,7 @@ import me.tatarka.inject.annotations.Inject
 @EventScope
 class EventHistoryScreenViewModel(
     private val eventUseCases: EventUseCases
-    ) : ViewModel() {
+    ) : SideEffectViewModel<EventHistoryScreenEvent.SideEffectEvent>() {
         val state = EventHistoryScreenState()
 
         val eventHistoryFlow: SharedFlow<Unit> = eventUseCases.getHistory()
@@ -27,13 +27,13 @@ class EventHistoryScreenViewModel(
                 SharingStarted.WhileSubscribed(5000)
             )
 
-//    fun onEvent(event: EventHistoryScreenEvent) {
-//        // this must be exhaustive - if you delete one of the items (lines), you'll see
-//        // that it shows an error and won't let you compile
-//        when (event) {
-//            is EventHistoryScreenEvent.StartTracking -> startTracking(event.id)
-//        }
-//    }
+    fun onEvent(event: EventHistoryScreenEvent) {
+        // this must be exhaustive - if you delete one of the items (lines), you'll see
+        // that it shows an error and won't let you compile
+        when (event) {
+            is EventHistoryScreenEvent.SideEffectEvent.NavigateToScreen999 -> emitSideEffect(event)
+        }
+    }
 //
 //    private fun startTracking(id: Long) {
 //        eventUseCases.start_tracking(id)
